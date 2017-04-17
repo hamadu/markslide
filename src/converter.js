a35e7e7b77aa18981b01e70e3d1bcce2ed309259
hamadu <dercll1020@gmail.com>
2017-04-17 20:56:19 +0900

    the first page is for meta information(title, style, etc...)

--- src/converter.js
+++ src/converter.js
@@ -5,17 +5,33 @@ const marked = require('marked');
 const fs = require('fs');
 const mustache = require('mustache');
 
+const encoding = { encoding: 'utf8' };
+
 const options = args([
   { name: 'verbose', alias: 'v', type: Boolean },
   { name: 'in', type: String, defaultValue: './sample.md' },
   { name: 'out', type: String }
 ]);
 
-const input = fs.readFileSync(options['in'], { encoding: 'utf8' });
-const contents = input.split('---').map((page, index) => {
+const input = fs.readFileSync(options['in'], encoding);
+const pages = input.split('---');
+
+const meta = {};
+pages[0].split('\n').forEach(row => {
+  const kv = row.split(':');
+  if (kv.length >= 2) {
+    meta[kv[0].trim()] = kv[1].trim();
+  }
+});
+
+const contents = pages.slice(1).map((page, index) => {
   return { id: page, contents: marked(page) }
 });
-const template = fs.readFileSync('./templates/template.html', { encoding: 'utf8' });
 
-const output = mustache.render(template, { contents: JSON.stringify(contents) });
+const htmlTemplate = fs.readFileSync('./templates/template.html', encoding);
+const cssTemplate = fs.readFileSync('./templates/style.css', encoding);
+
+const css = mustache.render(cssTemplate, { background: meta['background'] || '#fff' });
+const output = mustache.render(htmlTemplate, { title: meta['title'] || 'title', contents: JSON.stringify(contents), css: css });
+
 process.stdout.write(output);
