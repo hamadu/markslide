1934334a9b85a86be8bd9a84f9e1f114c71ed247
hamadu <dercll1020@gmail.com>
2017-04-26 00:16:35 +0900

    template per page

--- src/converter.js
+++ src/converter.js
@@ -15,15 +15,56 @@ const options = args([
 ]);
 
 const input = fs.readFileSync(options['in'], encoding);
-const pages = input.split('---');
 
-const meta = {};
-pages[0].split('\n').forEach(row => {
-  const kv = row.split(':');
-  if (kv.length >= 2) {
-    meta[kv[0].trim()] = kv[1].trim();
+function parsePage(page) {
+  const lines = page.split('\n').filter(row => row.length >= 1);
+  const pageInfo = {};
+  const isKeyStart = lines.map(line => line.match(/^[a-zA-Z_]+:/));
+
+  if (isKeyStart[0] !== null) {
+    for (let i = 0 ; i < lines.length ; ) {
+      const keyLen = isKeyStart[i][0].length;
+      const key = isKeyStart[i][0].substr(0, keyLen-1);
+      const values = [lines[i].substr(keyLen).trim()];
+      let j = i+1;
+      while (j < lines.length && !isKeyStart[j]) {
+        values.push(lines[j]);
+        j++;
+      }
+      pageInfo[key] = values.join('\n').trim();
+      i = j;
+    }
+    return pageInfo;
+  } else {
+    return {
+      title: '',
+      template: 'default',
+      body: page
+    }
   }
-});
+}
+
+function makeContents(pageInfo) {
+  let html = '';
+
+  const markdownBody = pageInfo.body;
+  switch (pageInfo.template) {
+    case 'title':
+      html = '<div class="container"><h1 id="title">' + pageInfo.title + '</h1></div>';
+      break;
+    case 'default':
+      html = '<div class="container">' + parseKatex(marked(pageInfo.body)) + '</div>';
+      break;
+    case 'twopane':
+      const title = '<h1>' + pageInfo.title + '</h1>'
+      const left = '<div class="twopane_left">' + parseKatex(marked(pageInfo.bodyLeft)) + '</div>';
+      const right = '<div class="twopane_right">' + parseKatex(marked(pageInfo.bodyRight)) + '</div>';
+
+      html = title + '<div class="container">' + left + right + '</div>';
+      break;
+  }
+  return html;
+}
 
 function parseKatex(page) {
   let pageWithMath = page;
@@ -36,15 +77,15 @@ function parseKatex(page) {
   return pageWithMath;
 }
 
-const titlePage = {
-  id: 0,
-  contents: '<div class="container"><h1 id="title">' + meta['title'] + '</h1></div>'
-};
-
-const contentPages = pages.slice(1).map((page, index) => {
-  return { id: page, contents: '<div class="container">' + parseKatex(marked(page)) + '</div>' }
+const pages = input.split('---').map(page => parsePage(page));
+const contents = pages.map((page, index) => {
+  return { id: index, contents: makeContents(page) }
 });
-const contents = [titlePage].concat(contentPages);
+
+const meta = {
+  title: pages[0].title,
+  background: pages[0].background
+};
 
 const htmlTemplate = fs.readFileSync('./templates/template.html', encoding);
 const cssTemplate = fs.readFileSync('./templates/style.css', encoding);
